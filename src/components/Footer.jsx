904227c1ef747326e25764435532d31c1d40d750
Alan Albiero <alan.albiero@gmail.com>
2020-07-08 18:12:09 -0300

    Adicionado aviso finalizar site

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import './Footer.css';
 
 export default props => (
-    <div className="footer">
-        <p>Copyright Pokemon Store @ 2020</p>
-    </div>
+    <>
+        <div className="aviso">
+            <p>Finalizando...
+                site estático em React
+            </p>
+        </div>
+        <div className="footer">
+            <p>Copyright Pokemon Store @ 2020</p>
+        </div>
+    </>
 )
